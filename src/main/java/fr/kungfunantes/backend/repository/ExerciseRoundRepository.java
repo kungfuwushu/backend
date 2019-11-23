308ca1f6a9c35e0a23213658ff0840f8848da182
Jonas Delannoy <e165556c@etu.univ-nantes.fr>
2019-11-23 14:15:36 +0100

    Started link between front/back for the deletion of an exercise

--- src/main/java/fr/kungfunantes/backend/repository/ExerciseRoundRepository.java
+++ src/main/java/fr/kungfunantes/backend/repository/ExerciseRoundRepository.java
@@ -1,5 +1,6 @@
 package fr.kungfunantes.backend.repository;
 
+import fr.kungfunantes.backend.model.round.Round;
 import fr.kungfunantes.backend.model.exercise.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,11 +9,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
+public interface ExerciseRoundRepository extends JpaRepository<Round, Long>{
   @Modifying
-  @Query("delete from Exercise where id = :idDelete")
+  @Query("delete from Round where id = :idDelete")
   public void deleteById(@Param("idDelete") Long idDelete);
-
-  @Query("select type from Exercise where id = :idSearched")
-  public String findTypeById(@Param("idSearched") Long idSearched);
 }
--- src/main/java/fr/kungfunantes/backend/resource/ExerciseResource.java
+++ src/main/java/fr/kungfunantes/backend/resource/ExerciseResource.java
@@ -2,10 +2,15 @@ package fr.kungfunantes.backend.resource;
 
 import fr.kungfunantes.backend.model.exercise.Exercise;
 import fr.kungfunantes.backend.repository.ExerciseRepository;
+import fr.kungfunantes.backend.repository.ExerciseCriteriaRepository;
+import fr.kungfunantes.backend.repository.ExerciseRoundRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,9 +20,39 @@ public class ExerciseResource {
 	@Autowired
 	private ExerciseRepository exerciseRepository;
 
-    @GetMapping("/exercises")
-    @ResponseBody
-    public List<Exercise> all() {
-        return exerciseRepository.findAll();
+	@Autowired
+	private ExerciseCriteriaRepository exerciseCriteriaRepository;
+
+	@Autowired
+	private ExerciseRoundRepository exerciseRoundRepository;
+
+  @ResponseBody
+  public List<Exercise> all() {
+      return exerciseRepository.findAll();
 	}
+
+	@ResponseBody
+	public String getType(@RequestParam Long id) {
+	    return exerciseRepository.findTypeById(id);
+	}
+
+	@DeleteMapping("/exercises/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	@Transactional
+	public void delete(@PathVariable("id") Long id) {
+			String exerciseType = getType(id);
+			switch(exerciseType) {
+				case "TAOLU":
+					exerciseCriteriaRepository.deleteById(id);
+					exerciseRepository.deleteById(id);
+					break;
+				case "FIGHT":
+					exerciseRoundRepository.deleteById(id);
+					exerciseRepository.deleteById(id);
+					break;
+				default:
+					exerciseRepository.deleteById(id);
+			}
+	}
+
 }
